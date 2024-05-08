cbc628a6785f258d3510f4f4a10c6a63bd07ff2f
Law <lawrencedegoma@yahoo.com>
2024-05-08 11:18:13 -0700

    Commented out ugly binary search alg

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ void quickSort(int [], int);
 void partition(int [], int, int&);
 void merge(int [], int, int);
 int binarySearch(int [], int, int, int);
-void search(const int a[], std::size_t first, std::size_t size, int target, bool &found, int &location);
+//void search(const int a[], std::size_t first, std::size_t size, int target, bool &found, int &location);
 void print(int [], int);
 
 int main() {
@@ -146,24 +146,24 @@ void mergeSort(int data[], int n) {
 }
 
 void merge(int data[], int n1, int n2){
-	/*
-	 * int *temp; // Holds the sorted array
-	 * int copied = 0 // keep track of how many elements are copied to temp
-	 * int copied1 = 0 // keep track of how many elements were copied from the first half
-	 * int copied2 = 0 // keep track of how many elements were copied from the second half
-	 * 
-	 * dynamically create an array of size n1+n2
-	 * while we haven't copied everything from the arrays
-	 * 	if (data[copied1] < data[copied2])
-	 * 		temp[copied++] = data[copied1++]
-	 * put the remaining values in temp
-	 * while (copied1 < n1)
-	 * 	blah blah blah
-	 * while (copied2 < n2)
-	 * 	blah blah blah
-	 *
-	 * copy everything from temp back to data
-	 * delete temp
+    /*
+     * int *temp; // Holds the sorted array
+     * int copied = 0 // keep track of how many elements are copied to temp
+     * int copied1 = 0 // keep track of how many elements were copied from the first half
+     * int copied2 = 0 // keep track of how many elements were copied from the second half
+     *
+     * dynamically create an array of size n1+n2
+     * while we haven't copied everything from the arrays
+     * 	if (data[copied1] < data[copied2])
+     * 		temp[copied++] = data[copied1++]
+     * put the remaining values in temp
+     * while (copied1 < n1)
+     * 	blah blah blah
+     * while (copied2 < n2)
+     * 	blah blah blah
+     *
+     * copy everything from temp back to data
+     * delete temp
     */
 
     int *temp = new int[n1+n2];
@@ -206,29 +206,29 @@ int binarySearch(int data[], int target, int left, int right){
     return -1;
 }
 
-// The oh so ugly binary search approach that takes more arguments than God ever intended
-void search(const int a[], std::size_t first, std::size_t size, int target, bool &found, int, int &location){
-    // base case
-    if (size == 0)
-        found = false;
-
-    else{
-        // calculate middle index
-        std::size_t middleIndex = size / 2;
-        if (a[middleIndex] == target){
-            found = true;
-            location = middleIndex;
-        }
-        else {
-            if (target < a[middleIndex]) {
-                search(a, first, middleIndex, target, found, location);
-            }
-            else{
-                search(a, middleIndex+1, size-middleIndex, target, found, location);
-            }
-        }
-    }
-}
+//// The oh so ugly binary search approach that takes more arguments than God ever intended
+//void search(const int a[], std::size_t first, std::size_t size, int target, bool &found, int, int &location){
+//    // base case
+//    if (size == 0)
+//        found = false;
+//
+//    else{
+//        // calculate middle index
+//        std::size_t middleIndex = size / 2;
+//        if (a[middleIndex] == target){
+//            found = true;
+//            location = middleIndex;
+//        }
+//        else {
+//            if (target < a[middleIndex]) {
+//                search(a, first, middleIndex, target, found, location);
+//            }
+//            else{
+//                search(a, middleIndex+1, size-middleIndex, target, found, location);
+//            }
+//        }
+//    }
+//}
 
 void print(int data[], int size){
     for (int i=0; i<size; i++)
